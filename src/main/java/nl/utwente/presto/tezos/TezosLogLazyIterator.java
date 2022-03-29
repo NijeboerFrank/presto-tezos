1f400deb03ee1d9423dbe775e37bb639488dd4c3
Jaap-Meerhof <74102699+Jaap-Meerhof@users.noreply.github.com>
2022-03-29 12:23:42 +0200

    Update src/main/java/nl/utwente/presto/tezos/TezosLogLazyIterator.java
    sure, delete instead of comment //
    
    Co-authored-by: NijeboerFrank <44158679+NijeboerFrank@users.noreply.github.com>

--- src/main/java/nl/utwente/presto/tezos/TezosLogLazyIterator.java
+++ src/main/java/nl/utwente/presto/tezos/TezosLogLazyIterator.java
@@ -29,32 +29,6 @@ public class TezosLogLazyIterator implements Iterator<Log> {
     @Override
     public boolean hasNext() {
         return true;
-//        if (logIter != null && logIter.hasNext()) {
-//            return true;
-//        }
-//
-//        while (txIter.hasNext()) {
-//            EthBlock.TransactionResult tr = txIter.next();
-//            EthBlock.TransactionObject tx = (EthBlock.TransactionObject) tr.get();
-//            try {
-//                log.info("Getting tx receipts...");
-//                Optional<TransactionReceipt> transactionReceiptOptional = tezosClient.getTransaction(tx.getHash()) // actual transaction
-//                        .send()
-//                        .getTransactionReceipt()
-//                        .filter(receipt -> receipt.getLogs() != null && !receipt.getLogs().isEmpty());
-//                if (!transactionReceiptOptional.isPresent()) {
-//                    continue;
-//                }
-//
-//                this.logIter = transactionReceiptOptional.get().getLogs().iterator();
-//                return true;
-//            } catch (IOException e) {
-//                throw new IllegalStateException("Unable to get transactionReceipt");
-//            }
-//
-//        }
-//
-//        return false;
     }
 
     @Override
