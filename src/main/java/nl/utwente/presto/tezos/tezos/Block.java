079944989f816e053ecb37461aeacd39e204e31d
Wilco van Beijnum <wilcovanbeijnum@gmail.com>
2022-03-31 15:05:32 +0200

    Add noarg constructor to block for jackson

--- src/main/java/nl/utwente/presto/tezos/tezos/Block.java
+++ src/main/java/nl/utwente/presto/tezos/tezos/Block.java
@@ -4,12 +4,14 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.time.Instant;
 
 @Getter
 @Setter
+@NoArgsConstructor
 @AllArgsConstructor
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class Block {
