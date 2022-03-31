7cd7fc355aa3f2f620996a9fcee39773aece6c00
Wilco van Beijnum <wilcovanbeijnum@gmail.com>
2022-03-31 15:11:15 +0200

    Fix injection of client provider

--- src/main/java/nl/utwente/presto/tezos/tezos/TezosClientProvider.java
+++ src/main/java/nl/utwente/presto/tezos/tezos/TezosClientProvider.java
@@ -1,18 +1,18 @@
 package nl.utwente.presto.tezos.tezos;
 
+import com.google.inject.Inject;
 import nl.utwente.presto.tezos.connector.TezosConnectorConfig;
 
 public class TezosClientProvider {
 
     private final TezosClient tezosClient;
 
+    @Inject
     public TezosClientProvider(TezosConnectorConfig config) {
-
         tezosClient = new TezosClient(config.getTezosEndpoint());
     }
 
     public TezosClient getTezosClient() {
-
         return this.tezosClient;
     }
 }
