f24bed9d71d5d149b62e3e6c1b200329bc29deec
Wilco van Beijnum <wilcovanbeijnum@gmail.com>
2022-04-07 11:25:25 +0200

    Add docs

--- src/main/java/nl/utwente/presto/tezos/TezosSplit.java
+++ src/main/java/nl/utwente/presto/tezos/TezosSplit.java
@@ -11,11 +11,16 @@ import java.util.Collections;
 import java.util.List;
 
 public class TezosSplit implements ConnectorSplit {
-
     private final TezosTable table;
     private final Type type;
     private final Object value;
 
+    /**
+     * Create a new split
+     * @param table table of the split
+     * @param type type of the split
+     * @param value value of the split
+     */
     @JsonCreator
     public TezosSplit(
             @JsonProperty("table") TezosTable table,
@@ -26,43 +31,85 @@ public class TezosSplit implements ConnectorSplit {
         this.value = value;
     }
 
+    /**
+     * Create a new split for a block
+     * @param blockId block height (ID)
+     * @return new split
+     */
     public static TezosSplit forBlock(long blockId) {
         return new TezosSplit(TezosTable.BLOCK, Type.BLOCK, blockId);
     }
 
+    /**
+     * Create a new split for a proposal
+     * @param proposalId proposal ID
+     * @return new split
+     */
     public static TezosSplit forProposal(long proposalId) {
         return new TezosSplit(TezosTable.ELECTION, Type.PROPOSAL, proposalId);
     }
 
+    /**
+     * Create a new split for an election
+     * @param electionId election ID
+     * @return new split
+     */
     public static TezosSplit forElection(long electionId) {
         return new TezosSplit(TezosTable.ELECTION, Type.ELECTION, electionId);
     }
 
+    /**
+     * Get the table of the split
+     * @return split table
+     */
     @JsonProperty
     public TezosTable getTable() {
         return table;
     }
 
+    /**
+     * Get the type of the split
+     * @return split type
+     */
     @JsonProperty
     public  Type getType() {
         return type;
     }
 
+    /**
+     * Get the value of the split
+     * @return split value
+     */
     @JsonProperty
     public Object getValue() {
         return value;
     }
 
+    /**
+     * Get block height (ID) of split
+     * @return block height (ID)
+     * @throws IllegalArgumentException if split type is not for block
+     */
     public long getBlockId() {
         if (type != Type.BLOCK) throw new IllegalArgumentException();
         return Long.parseLong(value.toString());
     }
 
+    /**
+     * Get election id of split
+     * @return election id
+     * @throws IllegalArgumentException if split type is not for election
+     */
     public long getElectionId() {
         if (type != Type.ELECTION) throw new IllegalArgumentException();
         return Long.parseLong(value.toString());
     }
 
+    /**
+     * Get proposal id of split
+     * @return proposal id
+     * @throws IllegalArgumentException if split type is not for proposal
+     */
     public long getProposalId() {
         if (type != Type.PROPOSAL) throw new IllegalArgumentException();
         return Long.parseLong(value.toString());
@@ -83,6 +130,9 @@ public class TezosSplit implements ConnectorSplit {
         return this;
     }
 
+    /**
+     * Enum of possible split types
+     */
     enum Type {
         BLOCK,
         ELECTION,
