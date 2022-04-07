d50479289068e5448089153ae9ec404767760dbf
Wilco van Beijnum <wilcovanbeijnum@gmail.com>
2022-04-07 12:45:39 +0200

    Undo changes to pair

--- src/main/java/nl/utwente/presto/tezos/Pair.java
+++ src/main/java/nl/utwente/presto/tezos/Pair.java
@@ -1,26 +1,12 @@
 package nl.utwente.presto.tezos;
 
-import com.fasterxml.jackson.annotation.JsonCreator;
-import com.fasterxml.jackson.annotation.JsonProperty;
-
 public final class Pair<T, U> {
     public final T first;
     public final U second;
 
-    @JsonCreator
-    public Pair(@JsonProperty("first") T first, @JsonProperty("second") U second) {
+    public Pair(T first, U second) {
         this.second = second;
         this.first = first;
-    }
-
-    @JsonProperty
-    public T getFirst() {
-        return first;
-    }
-
-    @JsonProperty
-    public U getSecond() {
-        return second;
     }
 }
 
